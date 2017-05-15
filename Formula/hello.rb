80a702c6a563c6640a4b00ed38b2094533829f9b
Carter Sande <carter.sande@duodecima.technology>
2017-05-15 16:41:22 -0400

    Re-add hello

---
+++ Formula/hello.rb
@@ -0,0 +1,21 @@
+class Hello < Formula
+  desc "Produces a familiar, friendly greeting."
+  homepage "https://www.gnu.org/software/hello/"
+  url "https://ftp.gnu.org/gnu/hello/hello-2.10.tar.gz"
+  sha256 "31e066137a962676e89f69d1b65382de95a7ef7d914b8cb956f41ea72e0f516b"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/hello"
+    system "#{bin}/hello", "-t"
+    system "#{bin}/hello", "-g", "Homebrew!"
+  end
+end
